678b9ad1cacda779c75968747ea74b791a11288d
Dom Kim <dhk2898@hotmail.com>
2025-07-17 12:19:37 +0900

    refactoring with reducer

--- Repos/clicker/src/components/CounterManager.tsx
+++ Repos/clicker/src/components/CounterManager.tsx
@@ -1,53 +1,50 @@
-import {useState} from 'react'
+import {useState, useReducer, useRef} from 'react'
 import CounterList from './CounterList.tsx'
 
-let nextId = 0;
 const CounterManager = () => {
+    let nextId = useRef(0);
     const initialCounters = [];
-    const [counters, setCounters] = useState(initialCounters);
     const [total, setTotal] = useState(0);
-
+    const [counters, dispatch] = useReducer(counterReducer, initialCounters)
 
     function handleAddCounter(){
-        setCounters([
-            ...counters,
-            {
-                id: nextId++,
-                value:0
-            }
-        ])
+        dispatch({
+            type: "counter-instance-added",
+            newId: nextId.current++
+        })
     }
 
     function handleRemoveCounter(){
-        setCounters(prev => prev.slice(0, -1));
+        dispatch({
+            type: "counter-instance-removed"
+        })
     }
 
     function handleCounterChange(id, amount){
-        setCounters(prev => prev.map(
-            counter => counter.id === id ? 
-            {...counter, value: counter.value + amount} : counter
-        ))
+        dispatch({
+            type: "counter-value-changed",
+            id,
+            amount 
+        });
     }
 
     function handleGlobalCounterChange(amount){
-        setCounters(prev => prev.map(counter => ({
-            ...counter,
-            value: counter.value + amount
-        })))
+        dispatch({
+            type: "counter-value-changed-global",
+            amount
+        })
     }
 
-    function handleRandomize(){
-        setCounters(prev => prev.map(counter => ({
-            ...counter,
-            value: Math.floor(Math.random() * 10)
-        })))
+    function handleRandomize() {
+        dispatch({
+            type: "counter-value-changed-global-random"
+        })
     }
 
-     function handleReset(){
-        setCounters(prev => prev.map(counter => ({
-            ...counter,
-            value: 0
-        })))
+    function handleReset(){
+        dispatch({
+            type: "counter-value-reset-global"
+        })
     }
 
     return(
@@ -63,6 +60,47 @@ const CounterManager = () => {
         </>
     )
     
+}
+
+function counterReducer(counters, action){
+    switch(action.type){
+        case "counter-value-changed": {
+            return counters.map(counter =>
+                counter.id === action.id
+                ? { ...counter, value: counter.value + action.amount }
+                : counter
+            );
+        }
+        case "counter-value-changed-global": {
+            return counters.map(counter =>
+                ({ ...counter, value: counter.value + action.amount })
+            );
+        }
+        case "counter-value-changed-global-random":{
+            return counters.map(counter =>
+                ({ ...counter, value: Math.floor(Math.random()*10) })
+            );
+        }
+        case "counter-value-reset-global":{
+            return counters.map(counter =>
+                ({ ...counter, value: 0})
+            );
+        }
+        case "counter-instance-added":{
+            return ([
+            ...counters,
+            {
+                id: action.newId,
+                value:0
+            }
+            ]);
+        }
+        case "counter-instance-removed":{
+            return counters.slice(0, -1);
+        }
+        default:
+            return counters;
+    }
 }
 
 
